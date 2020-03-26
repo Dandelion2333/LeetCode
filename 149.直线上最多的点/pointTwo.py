3e23b35929fbf49ba65375fbf812ebcb83f45511
Dandelion <494933900@qq.com>
2020-03-26 20:21:38 +0800

    完成第149题，直线上最多的点

--- 149.直线上最多的点/pointTwo.py
+++ 149.直线上最多的点/pointTwo.py
@@ -1,99 +1,101 @@
-class Solution:
-    def maxPoints(self, points):
-        points, deleteNum = Solution.RemoveDuplicateElements(points)
-
-        kList = []
-        pList = []
-        disposeList = []
-        if len(points) > 1:
-            for leftCnt in range (len(points)):
-                rightCnt = leftCnt + 1
-                while rightCnt < (len(points)):
-                    k = Solution.GetSlope(points[leftCnt][0], points[leftCnt][1], points[rightCnt][0], points[rightCnt][1])
-                    kList.append(k)
-                    pList.append(points[leftCnt])
-
-                    disposeList = Solution.DisposeK(k, points[leftCnt][0], points[leftCnt][1], disposeList)
-
-                    rightCnt = rightCnt + 1
-            
-            print("len(klist)",len(kList))
-            print(kList)
-            print(len(pList))
-            print(pList)
-            print("disposeList",disposeList)
-            kCount = Solution.GetKCount(disposeList)
-            # print(kCount)
-            # #print(max(kList, key=kList.count))
-            # kCount = kList.count(max(kList, key=kList.count))
-            print(kCount, kList)
-            kNum = Solution.GetKNum(kCount)
-            # #print(kCount, kNum, kList)
-        else:
-            if len(points) == 0:
-                kNum = 0
-            elif len(points) == 1:
-                kNum = 1
-
-        return (kNum + deleteNum)
-
-    def GetKCount(disposeList):
-        kCount = 0
+class Solution():
+    def mostPoint(self, points):
+        if len(points) == 0:
+            return 0
+        elif len(points) == 1:
+            return 1
+        elif (len(points) == 2):
+            return 2
         
-        for cnt in range (len(disposeList)):
-            if kCount < (disposeList[cnt][2]):
-                kCount = disposeList[cnt][2]
-            
-        return kCount
 
-    def DisposeK(k, x, y, disposeList):
-        flag = False
-        if k == 999999:
-            b = x
-        else:
-            b = round((y - k*x), 6)
+        maxNum = Solution().poll(points)  
 
-        for index in range (len(disposeList)):
-            if (k == disposeList[index][0]) and (b == disposeList[index][1]):
-                flag = True
-                disposeList[index][2] = disposeList[index][2] + 1
+        if maxNum == len(points):
+            maxNum = Solution().adjustMaxNum(points)
 
-        if flag == False:        
-            num = 1
-            disposeList.append([k, b, num])
+        return maxNum
+
+    def adjustMaxNum(self, points):
+        temp = points[1]
+        points[1] = points[0]
+        points[0] = temp
         
-        return disposeList
+        maxNum = Solution().poll(points)
 
-    def GetSlope(x1, y1, x2, y2):
-        if (x2 - x1) != 0:
-            k = (((y2 - y1) / (x2 - x1)))
+        return maxNum
+
+    def poll(self, points):
+        linePoint = []
+        leftCnt = 0
+        midCnt = 0
+        rightCnt = 0
+        maxNum = 0
+        while leftCnt < len(points):
+            linePoint = []
+            midCnt = leftCnt + 1
+            while midCnt < len(points):
+                Solution().createLine(points[leftCnt][0], points[leftCnt][1], points[midCnt][0], points[midCnt][1], linePoint)                
+                midCnt = midCnt +1
+            # 获取本轮中直线最多的点的个数
+            maxNumTemp = Solution().getMaxNum(linePoint)
+            if maxNumTemp >= maxNum:
+                maxNum = maxNumTemp
+            leftCnt = leftCnt + 1
+        
+        return maxNum
+
+    def createLine(self, x1, y1, x2, y2, linePoint):
+        if (x1== x2) and (y1 == y2):
+            k = 666666
+            b = 666666
         else:
-            k = 999999
+            if (x2 - x1) == 0:
+                k = 999999
+                b =  x1
+            elif (y2 - y1) == 0:
+                k = 0
+                b = y1
+            else:
+                k = (float)(((y2 - y1) / (x2 - x1)))
+                b = y1 - k * x1
+        #print("k b", k, b, x1, y1, x2, y2)
+        
+        Solution().judgeKB(linePoint, k, b)
 
-        return k
-    def GetKNum(kCount):
-        kNum = 0
-        while 1:
-            if (kNum * kNum - kNum) >= (kCount*2):
-                print(kNum)
+    def judgeKB(self, linePoint, k, b):
+
+        cnt = 0
+        while cnt < (len(linePoint)):
+            if linePoint[cnt][1] == k and linePoint[cnt][2] == b and k != 666666 and b != 666666:
+                linePoint[cnt][0] = linePoint[cnt][0] + 1
+                #print("addvalue k b", k, b)
                 break
-            
-            kNum = kNum + 1
-        return kNum
-    def RemoveDuplicateElements(list):
-        deleteNum = 0
-        for leftCnt in range (len(list)):
-            rightCnt = leftCnt + 1
-            while rightCnt < len(list):
-                if (list[leftCnt][0] == list[rightCnt][0]) and (list[leftCnt][1] == list[rightCnt][1]):
-                    del list[rightCnt]
-                    deleteNum = deleteNum + 1
-                rightCnt = rightCnt + 1
-        print(deleteNum)
-        return list, deleteNum
+            cnt = cnt + 1
+        if cnt >= len(linePoint):
+            linePoint.append([2, k, b])
+            #print("append k b", k, b)            
+
+    def getMaxNum(self, linePoint):
+        maxNum = 0
+        coincide = 0
+        # 获取最大值
+        for cnt in range (len(linePoint)):
+            if linePoint[cnt][0] >= maxNum:
+                maxNum = linePoint[cnt][0]
+            if linePoint[cnt][1] == 666666 and linePoint[cnt][2] == 666666:
+                coincide = coincide + 1
+
+        if coincide == len(linePoint):
+            # 表示数组中的点都是重合的
+            return len(linePoint) + 1
+        else:
+            return (maxNum + coincide)
+
 
 if __name__ == "__main__":
-    list = [[-435,-347],[-435,-347],[609,613],[-348,-267],[-174,-107],[87,133],[-87,-27],[-609,-507],[435,453],[-870,-747],[-783,-667],[0,53],[-174,-107],[783,773],[-261,-187],[-609,-507],[-261,-187],[-87,-27],[87,133],[783,773],[-783,-667],[-609,-507],[-435,-347],[783,773],[-870,-747],[87,133],[87,133],[870,853],[696,693],[0,53],[174,213],[-783,-667],[-609,-507],[261,293],[435,453],[261,293],[435,453]]
-    result = Solution().maxPoints(list)
+    points =  [[0,0],[0,0],[94911151,94911150],[94911152,94911151]]
+    
 
-    print(result)
+    num = Solution().mostPoint(points)
+
+    print(num)
